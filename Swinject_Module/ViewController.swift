9702058f7959a7d982728cb9e7194333a146dda1
BerezentesvEvgeny <jonn09@icloud.com>
2022-05-13 14:47:23 +0300

    Initial Commit

---
+++ Swinject_Module/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swinject_Module
+//
+//  Created by Евгений Березенцев on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
